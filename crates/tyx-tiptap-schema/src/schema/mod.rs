6ce534b33946bb366ca5eae6ad49b9cb028e2a64
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2025-05-10 22:21:43 +0800

    feat: generate tiptap schema for rust crates (#26)

---
+++ crates/tyx-tiptap-schema/src/schema/mod.rs
@@ -0,0 +1,73 @@
+pub use mark::*;
+pub use node::*;
+mod mark;
+mod node;
+mod prelude;
+
+use prelude::*;
+
+/// A TyX node.
+#[derive(Debug, Clone, Hash, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "camelCase")]
+pub enum TyxNode {
+    /// A mark.
+    Mark(TyxMark),
+    /// A `Blockquote` node.
+    Blockquote(Blockquote),
+    /// A `BulletList` node.
+    BulletList(BulletList),
+    /// A `CodeBlock` node.
+    CodeBlock(CodeBlock),
+    /// A `Doc` node.
+    Doc(Doc),
+    /// A `HardBreak` node.
+    HardBreak(HardBreak),
+    /// A `Heading` node.
+    Heading(Heading),
+    /// A `HorizontalRule` node.
+    HorizontalRule(HorizontalRule),
+    /// A `ListItem` node.
+    ListItem(ListItem),
+    /// A `MathBlock` node.
+    MathBlock(MathBlock),
+    /// A `MathInline` node.
+    MathInline(MathInline),
+    /// A `OrderedList` node.
+    OrderedList(OrderedList),
+    /// A `Paragraph` node.
+    Paragraph(Paragraph),
+    /// A `Table` node.
+    Table(Table),
+    /// A `TableCell` node.
+    TableCell(TableCell),
+    /// A `TableHeader` node.
+    TableHeader(TableHeader),
+    /// A `TableRow` node.
+    TableRow(TableRow),
+    /// A `Text` node.
+    Text(Text),
+}
+
+/// A TyX mark.
+#[derive(Debug, Clone, Hash, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "camelCase")]
+pub enum TyxMark {
+    /// A `Bold` node.
+    Bold(Bold),
+    /// A `Code` node.
+    Code(Code),
+    /// A `Highlight` node.
+    Highlight(Highlight),
+    /// A `Italic` node.
+    Italic(Italic),
+    /// A `Link` node.
+    Link(Link),
+    /// A `Strike` node.
+    Strike(Strike),
+    /// A `Subscript` node.
+    Subscript(Subscript),
+    /// A `Superscript` node.
+    Superscript(Superscript),
+    /// A `Underline` node.
+    Underline(Underline),
+}
---
+++ crates/tyx-tiptap-schema/src/schema/prelude.rs
@@ -0,0 +1,6 @@
+pub use super::{TyxMark, TyxNode};
+pub use ecow::EcoString;
+
+pub use serde::{Deserialize, Serialize};
+
+pub type Content = Vec<TyxNode>;
